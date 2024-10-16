ae19cf512bb303cf81d193511090ff70aeda7350
JUAN DIEGO QUIJADA CASTILLO <128547155+D3fussion@users.noreply.github.com>
2024-10-15 19:36:05 -0700

    Update get_db_connection.py

--- get_db_connection.py
+++ get_db_connection.py
@@ -2,5 +2,5 @@ import psycopg2
 
 
 def get_db_connection():
-    conn = psycopg2.connect("postgresql://DonBaraton_owner:FzYT6uwqL8ek@ep-wispy-bird-a4q98q3x.us-east-1.aws.neon.tech/DonBaraton?sslmode=require")
+    conn = psycopg2.connect("")
     return conn
